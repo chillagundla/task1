12ae17c90853eec575fb944d1d6362cd5f319a92
chillagundla <104352675+chillagundla@users.noreply.github.com>
2022-05-05 13:32:11 +0000

    task12

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python
 WORKDIR /app
 # We copy just the requirements.txt first to leverage Docker cache
 COPY ./requirements.txt .
-ENV DB_URL=DummyURL
+ENV DB_URL=mysql://admin:Sri_7999@mysql-database.c3xbx00f9wvt.ap-south-1.rds.amazonaws.com/aws
 
 #this runs when image is built
 RUN pip install -r requirements.txt
--- models/Dbperson.py
+++ models/Dbperson.py
@@ -1,22 +1,28 @@
 from config import db
 
 class DbPerson(db.Model):
-    __tablename__='people'
+    __tablename__='employee'
     sno=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(30),index=False,unique=False,nullable=False)
     city=db.Column(db.String(30),index=False,unique=False,nullable=False)
+    desig=db.Column(db.String(30),index=False,unique=False,nullable=False)
+    age=db.Column(db.integer(30),index=False,unique=False,nullable=False)
     
 
-    def __init__(self,sno,name,city):
+    def __init__(self,sno,name,city,desig,age):
         self.sno=sno
         self.name=name
         self.city=city
+        self.desig=desig
+        self.age=age
     
     def serialize(self):
         return {
             'sno':self.sno,
             'name':self.name,
             'city':self.city
+            'desig':self.desig
+            'age':self.age
             }
     
     def __repr__(self):
